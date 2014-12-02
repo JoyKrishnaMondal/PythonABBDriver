10d6c54014496c0838e090c175091e7df242727c
Joy <joykrishnamondal@gmail.com>
2014-12-02 15:22:43 +0000

    Ben's MatLab Code

--- MoveRobot.m
+++ MoveRobot.m
@@ -2,4 +2,8 @@ function MoveRobot(value)
  %  Value should be in the form of "0,0,0"
 system(horzcat('ipy DllAccess.py ',value));
 
+fileID = fopen('arg.txt','w');
+fprintf(fileID,value);
+f.close(fileID);
+
 end
